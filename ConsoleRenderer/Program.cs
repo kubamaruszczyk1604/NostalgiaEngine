0222f58fb5b23c48757f25c78a3003da6ada181d
Kuba Maruszczyk <kuba_maruszczyk@hotmail.co.uk>
2019-06-27 22:49:18 +0100

    Disabled console window resize

--- ConsoleRenderer/Program.cs
+++ ConsoleRenderer/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenTK;
-
+using System.Runtime.InteropServices;
 namespace ConsoleRenderer
 {
     class Sphere:RenderObject
@@ -117,8 +117,28 @@ namespace ConsoleRenderer
 
     class Program
     {
+        private const int MF_BYCOMMAND = 0x00000000;
+        public const int SC_CLOSE = 0xF060;
+        public const int SC_MINIMIZE = 0xF020;
+        public const int SC_MAXIMIZE = 0xF030;
+        public const int SC_VSCROLL = 0xF070;
+        public const int SC_HSCROLL = 0xF080;
+        public const int SC_SIZE = 0xF000;
+
+        [DllImport("user32.dll")]
+        public static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+
         static void Main(string[] args)
         {
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_MINIMIZE, MF_BYCOMMAND);
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_SIZE, MF_BYCOMMAND);
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_MAXIMIZE, MF_BYCOMMAND);
 
             LitMaterial greenLitMat;
             greenLitMat.ColorA = RayMarcher.BACKGROUND_GREEN;
@@ -144,9 +164,9 @@ namespace ConsoleRenderer
             unlitMat.ColorA = 0 ;
             unlitMat.ColorB = 0;
             unlitMat.BlockType = Block.Weak;
+           
 
-
-           RayMarcher rm = new RayMarcher(100, 40);
+            RayMarcher rm = new RayMarcher(100, 40);
         
 
             Sphere sphere = new Sphere(greenLitMat);
