2970a0deb3fc58b36ff23f9c48a82aa98b47525f
Kuba Maruszczyk <kmaruszczyk1604@gmail.com>
2021-12-23 18:02:17 +0000

    Started texture16 implementation

---
+++ ConsoleRenderer/Texture16.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRenderer
+{
+    class Texture16
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public string m_ErrorMsg { get; private set; }
+     
+
+        private int[] m_Data;
+
+
+
+        private Texture16(int w, int h)
+        {
+            m_Data = new int[w * h];
+        }
+
+        private bool ReadFromFile(string path)
+        {
+            try
+            {
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(path))
+                {
+
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+                return false;
+        }
+
+        public static bool LoadFromFile(string file, out Texture16 texture)
+        {
+            texture = null;
+            return false;
+        }
+
+        public ColorSample Sample(float u, float v, float intensity)
+        {
+            return null;
+        }
+
+    }
+}
