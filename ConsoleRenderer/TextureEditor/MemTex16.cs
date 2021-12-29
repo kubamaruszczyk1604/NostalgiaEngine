db104864aa40aa755caaa47437755bda52b4d10e
Kuba Maruszczyk <kmaruszczyk1604@gmail.com>
2021-12-29 21:34:43 +0000

    linked list for texture buffer

---
+++ ConsoleRenderer/TextureEditor/MemTex16.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRenderer.TextureEditor
+{
+    class MemTex16
+    {
+        public class MT16Pix
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Col { get; set; }
+            public int Metadata { get; set; }
+
+            public MT16Pix LEFT { get; set; }
+            public MT16Pix RIGHT { get; set; }
+            public MT16Pix DOWN { get; set; }
+            public MT16Pix UP { get; set; }
+
+            public MT16Pix(int x, int y, int col)
+            {
+                X = x;
+                Y = y;
+                Col = col;
+            }
+        }
+
+        MT16Pix[] m_Pixels;
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        int XY2I(int x, int y)
+        {
+            return (Width * y + x);
+        }
+
+        public MemTex16(int w, int h)
+        {
+            Width = w;
+            Height = h;
+            m_Pixels = new MT16Pix[w * h];
+            for(int y = 0; y < h;++y)
+            {
+                for(int x= 0; x<w;++x)
+                {
+                    MT16Pix pixel = new MT16Pix(x, y, 15);
+                    pixel.Metadata = XY2I(x, y);
+                    if ((y!=0))
+                    {
+                        pixel.UP = m_Pixels[XY2I(x, y - 1)];
+                        pixel.UP.DOWN = pixel;
+                    }
+                    if(x!=0)
+                    {
+                        pixel.LEFT = m_Pixels[XY2I(x - 1, y)];
+                        pixel.LEFT.RIGHT = pixel;
+                    }
+
+                    m_Pixels[XY2I(x,y)] = pixel;
+                }
+            }
+        }
+
+        public int GetColor(int x, int y)
+        {
+            return m_Pixels[XY2I(x, y)].Col;
+        }
+        public MT16Pix GetPixel(int x, int y)
+        {
+            return m_Pixels[XY2I(x, y)];
+        }
+
+    }
+}
---
+++ ConsoleRenderer/TextureEditor/Test_MemTex16.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleRenderer.Core;
+using ConsoleRenderer;
+
+namespace ConsoleRenderer.TextureEditor
+{
+    class Test_MemTex16: CGApp
+    {
+        MemTex16 m_MemTex16;
+        public override void OnInitialize()
+        {
+            ScreenWidth = 20;
+            ScreenHeight = 20;
+            PixelWidth = 20;
+            PixelHeight = 20;
+            m_MemTex16 = new MemTex16(20, 10);
+            //m_MemTex16.GetPixel(1, 1).UP.Col = 14;
+            m_Current = m_MemTex16.GetPixel(1, 1);
+        }
+        public MemTex16.MT16Pix m_Current = null;
+        public override void OnStart() { }
+        public override void OnUpdate(float deltaTime)
+        {
+            if(CGInput.CheckKeyPress(ConsoleKey.DownArrow))
+            {
+                if(m_Current.DOWN != null)
+                {
+                    m_Current = m_Current.DOWN;
+                }
+            }
+            if (CGInput.CheckKeyPress(ConsoleKey.UpArrow))
+            {
+                if (m_Current.UP!= null)
+                {
+                    m_Current = m_Current.UP;
+                }
+            }
+            if (CGInput.CheckKeyPress(ConsoleKey.LeftArrow))
+            {
+                if (m_Current.LEFT != null)
+                {
+                    m_Current = m_Current.LEFT;
+                }
+            }
+            if (CGInput.CheckKeyPress(ConsoleKey.RightArrow))
+            {
+                if (m_Current.RIGHT != null)
+                {
+                    m_Current = m_Current.RIGHT;
+                }
+            }
+        }
+        public override void OnDrawPerColumn(int x) { }
+        public override void OnPostDraw()
+        {
+            for(int x = 0; x < m_MemTex16.Width;++x)
+            {
+                for(int y = 0; y< m_MemTex16.Height;++y)
+                {
+                    MemTex16.MT16Pix pix = m_MemTex16.GetPixel(x, y);
+                    bool d = pix.Metadata == m_Current.Metadata;
+                    CGBuffer.AddAsync( d?'X':'0', (short)(d?10:4), x, y);
+                }
+            }
+        }
+        public override void OnExit() { }
+    }
+}
