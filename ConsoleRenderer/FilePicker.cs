facbec56560a61d8a97abc23091e18ac3b8d13a4
Kuba Maruszczyk <kmaruszczyk1604@gmail.com>
2021-12-31 22:16:28 +0000

    file picker directory explorer

--- ConsoleRenderer/FilePicker.cs
+++ ConsoleRenderer/FilePicker.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ConsoleRenderer.Core;
+using System.IO;
 
 namespace ConsoleRenderer
 {
@@ -20,12 +21,43 @@ namespace ConsoleRenderer
         public override void OnStart() { }
         public override void OnPause() { }
         public override void OnResume() { }
-        public override void OnUpdate(float deltaTime) { }
+        public override void OnUpdate(float deltaTime)
+        {
+            if(CGInput.CheckKeyPress(ConsoleKey.DownArrow))
+            {
+                current++;
+            }
+            if (CGInput.CheckKeyPress(ConsoleKey.UpArrow))
+            {
+                current--;
+            }
+
+        }
         public override void OnDrawPerColumn(int x) { }
+        string m_CurrentDir = @"c:\";
+        int current = 1;
         public override void OnDraw()
         {
 
-            CGBuffer.WriteXY(0, 10, 12, "FILE PICKER TEMPLATE");
+            CGBuffer.WriteXY(0, 1, 12, "FILE PICKER TEMPLATE");
+            string[] files = Directory.GetFiles(m_CurrentDir);
+            int cnt = 1;
+
+            string[] dirs = Directory.GetDirectories(m_CurrentDir);
+
+            foreach (string dir in dirs)
+            {
+                CGBuffer.WriteXY(0, 2 + cnt, (short)(current==cnt?(11|1<<4):11), dir);
+                cnt++;
+            }
+
+            foreach (string file in files)
+            {
+                CGBuffer.WriteXY(0,2+cnt, 12, file);
+                cnt++;
+            }
+
+
         }
         public override void OnExit()
         {
