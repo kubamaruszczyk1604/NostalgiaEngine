35559092511e32a07e721f471ec5a90ece3eea4f
Kuba Maruszczyk <kmaruszczyk1604@gmail.com>
2022-01-01 20:21:23 +0000

    fixed exception on file access denied

--- ConsoleRenderer/FilePicker.cs
+++ ConsoleRenderer/FilePicker.cs
@@ -15,6 +15,9 @@ namespace ConsoleRenderer
         private readonly int c_DistanceBetweenColumns = 40;
 
         private Stack<string> m_DirStack;
+        string[] m_DirectoryList;
+        private string m_CurrentPath = "c:/";
+        private int m_CurrentPosIndex = 0;
 
         public override void OnInitialize()
         {
@@ -24,10 +27,14 @@ namespace ConsoleRenderer
             PixelWidth = 8;
             PixelHeight = 16;
             m_DirStack = new Stack<string>();
+            if(TryObtainDirList(m_CurrentPath,out string[] dirList))
+            {
+                m_DirectoryList = dirList;
+            }
+            
         }
-        public override void OnStart() { }
-        public override void OnPause() { }
-        public override void OnResume() { }
+
+
         public override void OnUpdate(float deltaTime)
         {
             if(CGInput.CheckKeyPress(ConsoleKey.DownArrow))
@@ -39,57 +46,67 @@ namespace ConsoleRenderer
                 m_CurrentPosIndex--;
             }
 
+
+            if (CGInput.CheckKeyPress(ConsoleKey.Enter))
+            {
+                string bk = m_DirectoryList[m_CurrentPosIndex].Substring(m_CurrentPath.Length);
+                if (bk == "...")
+                {
+                    if (m_CurrentPath.Length > 3)
+                    {
+                        string del = m_DirStack.Pop();
+                        m_CurrentPath = m_CurrentPath.Substring(0, m_CurrentPath.Length - del.Length - 1);
+                        TryObtainDirList(m_CurrentPath, out string[] dirList);
+                        m_DirectoryList = dirList;
+                        m_CurrentPosIndex = 0;
+                    }
+                }
+                else
+                {
+
+                    string newPath = m_DirectoryList[m_CurrentPosIndex] + "/";
+                    if (TryObtainDirList(newPath, out string[] dirList))
+                    {
+                        m_DirStack.Push(bk);
+                        m_CurrentPath = newPath;
+                        m_DirectoryList = dirList;
+                        m_CurrentPosIndex = 0;
+                    }
+
+                }
+            }
+
         }
-        public override void OnDrawPerColumn(int x) { }
-        string m_CurrentDirStr = "c:/";
-        int m_CurrentPosIndex = 0;
+        
+
+
+
+
 
         public override void OnDraw()
         {
             CGBuffer.Clear();
             CGBuffer.WriteXY(0, 0, 12, "FILE PICKER TEMPLATE");
-            CGBuffer.WriteXY(0, 1, 10, m_CurrentDirStr);
-            string[] files = Directory.GetFiles(m_CurrentDirStr);
-            int cnt = 0;
-
-            string[] dirs = Directory.GetDirectories(m_CurrentDirStr);
-
-            var temp = dirs.ToList();
-            //temp.AddRange(files);
-            temp.Insert(0, m_CurrentDirStr + "...");
-            dirs = temp.ToArray();
-
+            CGBuffer.WriteXY(0, 1, 10, m_CurrentPath);
+  
+            //int cnt = 0;
             int start = 0;
-            if (m_CurrentPosIndex >= 2*c_ColLength) start = c_ColLength;
-            for (int i = start; i < dirs.Length; ++i)
-            {
-                int x = (cnt / c_ColLength) * c_DistanceBetweenColumns;
-                
-                if(x< ScreenWidth)
-                CGBuffer.WriteXY(x, 3 + (cnt%c_ColLength), (short)(m_CurrentPosIndex-start==cnt?(11|1<<4):11), dirs[i].Substring(m_CurrentDirStr.Length));
-                cnt++;
-            }
-            if (m_CurrentPosIndex < 0) m_CurrentPosIndex = 0;
-            if (m_CurrentPosIndex >=  dirs.Length) m_CurrentPosIndex = dirs.Length-1;
-            if (CGInput.CheckKeyPress(ConsoleKey.Enter))
-            {
-                string bk = dirs[m_CurrentPosIndex].Substring(m_CurrentDirStr.Length);
-                if (bk == "...")
-                {
-                    if (m_CurrentDirStr.Length > 3)
-                    {
-                        string del = m_DirStack.Pop();
-                        m_CurrentDirStr = m_CurrentDirStr.Substring(0, m_CurrentDirStr.Length - del.Length - 1);
-                    }
-                }
-                else
-                {
-                    m_CurrentDirStr = dirs[m_CurrentPosIndex] + "/";
-                    m_DirStack.Push(bk);
-                }
-                    m_CurrentPosIndex = 0;
 
+            if (m_CurrentPosIndex >= 2*c_ColLength) start = c_ColLength;
+            for (int i = start; i < m_DirectoryList.Length; ++i)
+            {
+                int x = (i / c_ColLength) * c_DistanceBetweenColumns;
+                
+                if(x < ScreenWidth)
+                CGBuffer.WriteXY(x, 3 + (i%c_ColLength), (short)(m_CurrentPosIndex-start==i?(11|1<<4):11), m_DirectoryList[i].Substring(m_CurrentPath.Length));
+               // cnt++;
             }
+
+            if (m_CurrentPosIndex < 0) m_CurrentPosIndex = 0;
+            if (m_CurrentPosIndex >=  m_DirectoryList.Length) m_CurrentPosIndex = m_DirectoryList.Length-1;
+
+
+ 
             //foreach (string file in files)
             //{
             ////    CGBuffer.WriteXY(0,2+cnt, 12, file);
@@ -100,6 +117,35 @@ namespace ConsoleRenderer
         }
         public override void OnExit()
         {
+
+        }
+
+
+
+        private bool TryObtainDirList(string path, out string[] directoryList)
+        {
+            try
+            {
+
+                directoryList = Directory.GetDirectories(path);
+                for (int i = 0; i < directoryList.Length; ++i)
+                {
+                    directoryList[i] = directoryList[i].ToUpper();
+                }
+
+                var temp = directoryList.ToList();
+                string[] files = Directory.GetFiles(path);
+                temp.AddRange(files);
+                temp.Insert(0, path + "...");
+                directoryList = temp.ToArray();
+            }
+            catch
+            {
+                directoryList = null;
+                return false;
+            }
+            return true;
+
 
         }
     }
